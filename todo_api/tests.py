9dd37bff49c44d273661c101c294081cc5287616
jonathanferreirar <jonathanferreirar@gmail.com>
2023-11-04 10:04:19 -0300

    ":test_tube: test: create test to listodo class"

--- todo_api/tests.py
+++ todo_api/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Todo
 
-# Create your tests here.
+import json
+
+todo_to_create = {
+  "todo":"Todo created juts for test case"
+}
+todos_list_to_create = [
+  "Todo created juts for test case",
+  "Todo created juts for test case",
+  "Todo created juts for test case",
+  "Todo created juts for test case"
+]
+class ListTodoTest(APITestCase):
+  def test_create_todo(self):
+    
+    create_response = self.client.post("/api/todo/", data=todo_to_create, format='json')
+  
+    self.assertEqual(create_response.status_code, 201)
+    self.assertEqual(Todo.objects.count(), 1)
+    self.assertEqual(Todo.objects.get().todo, todo_to_create["todo"])
+    
+  def test_list_todo(self):
+    for todo in todos_list_to_create:
+      Todo.objects.create(
+        todo=todo
+      )     
+    list_response = self.client.get("/api/todo/", format='json')
+    data = json.loads(list_response.content)
+    
+    self.assertEqual(list_response.status_code, 200)
+    self.assertEqual(len(data), 4)
+    self.assertEqual(data[0]['todo'], todos_list_to_create[0])
+    
+
+class DetailTodoTest(APITestCase):
+  def test_list_one_todo(self):
+
+    
+
--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from .views import CreateTodo, DeleteTodo, ListTodo, UpdateTodo, DetailTodo
+from .views import ListTodo, DetailTodo
 
 
 urlpatterns = [
-    # Routers about user auth
+    
     path('todo/', ListTodo.as_view(), name='list_todo'),
     path('todo/<int:pk>', DetailTodo.as_view(), name='detail_todo'),
-    path('todo/add/', CreateTodo.as_view(), name='todo_add'),
-    path('todo/del/<int:pk>', DeleteTodo.as_view(), name='todo_delete'),
-    path('todo/upd/<int:pk>', UpdateTodo.as_view(), name='todo_update'),
-
-
-]
+  ]
