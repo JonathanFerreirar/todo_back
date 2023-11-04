74d7d69cb9994e2cd9b32f0e6b09c17a4ff977e3
jonathanferreirar <jonathanferreirar@gmail.com>
2023-11-04 09:32:23 -0300

    ":recycle: refactor: finish todo refactor"

--- todo_api/views.py
+++ todo_api/views.py
@@ -2,8 +2,7 @@ from .models import Todo
 from .serializers import TodoSerializer
 
 from rest_framework import mixins
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 
 
 class ListTodo(
@@ -35,59 +34,7 @@ class DetailTodo(
     def post (self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
+    def delete (self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
-class CreateTodo(generics.CreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = TodoSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            todo = serializer.data.get('todo')
-            create_at = serializer.data.get('create_at')
-            updated_at = serializer.data.get('updated_at')
-
-            return Response({
-                "data": {
-                    "todo": todo,
-                    "create_at": create_at,
-                    "updated_at": updated_at
-                }
-            }, status=status.HTTP_201_CREATED)
-        else:
-            return Response({
-                "message": "failed", "details": serializer.errors
-            })
-
-
-class UpdateTodo(generics.UpdateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        return Response({"message": f"'{instance.todo}' was updated successfully"}, status=status.HTTP_200_OK)
-
-
-class DeleteTodo(generics.DestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
-
-    def destroy(self, request, pk, *args, **kwargs):
-        instance = self.get_object()
-        todo = instance.todo
-
-        self.perform_destroy(instance)
-
-        return Response(
-            {"message": f" '{todo}' was deleted successfully"}, status=status.HTTP_200_OK
-        )
