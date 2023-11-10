190667b4a9e6688d97b379edf46cba231f4f27de
jonathanferreirar <jonathanferreirar@gmail.com>
2023-11-10 13:30:10 -0300

    :sparkles: feat: adjust serializer

--- todo_api/models.py
+++ todo_api/models.py
@@ -5,7 +5,7 @@ from user.models import User
 
 
 class Todo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     todo = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- todo_api/views.py
+++ todo_api/views.py
@@ -8,52 +8,28 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate
 
 
-class ListTodo(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView):
+class ListTodo(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     
     def get (self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return self.list(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
     
     def post (self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return self.create( request, *args, **kwargs)
+        return self.create( request, *args, **kwargs)
     
     
-class DetailTodo(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     
     def get (self, request, pk,*args, **kwargs):
-        if request.user.is_authenticated:
-            
-            todo = Todo.objects.get(id=pk)
-            print(todo)
-            
-            return Response({
-                "data": "ok"
-            })
-            #return self.retrieve(request, *args, **kwargs)
-        else:
-            return Response({
-                "error": "user is not authenticated"
-            })
-    
+       return self.retrieve(request, *args, **kwargs)
+      
     def put (self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return self.partial_update(request, *args, **kwargs)
+       return self.partial_update(request, *args, **kwargs)
 
     def delete (self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return self.destroy(request, *args, **kwargs)
+       return self.destroy(request, *args, **kwargs)
 
 
